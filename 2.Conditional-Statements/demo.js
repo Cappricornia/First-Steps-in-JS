a647ccb9711fd8bd981d8959920aa48e78057f14
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-08-13 14:42:10 +0300

    Update Conditional Statements

--- 2.Conditional-Statements/demo.js
+++ 2.Conditional-Statements/demo.js
@@ -1,1 +1,18 @@
+function colours(input){
+    let colour = input[0];
+    
+    if (colour === "red"){
+        console.log("tomato");
+        console.log("Colour of the winner!");
+     } else {
+        console.log("other colour"); 
+     }
+    console.log("Goodbye!");
+
+}
+
+colours(["red"]);
+
+
+
 
