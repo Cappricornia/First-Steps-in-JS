83bb797034c08b43a5159f311be0ebcfe911bd5f
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-08-13 14:43:32 +0300

    Update First Steps In JS

--- 1.First-Steps-In-JS/demoArrays.js
+++ 1.First-Steps-In-JS/demoArrays.js
@@ -1,1 +1,26 @@
+function demo() {
+    let numbers = [15, 8, 3];
+    console.log(numbers);
+    console.log(numbers[0]);
+    console.log("The length of the array is" + " " +  numbers.length);
+}
 
+demo();
+
+
+function solve(input){
+    console.log(input[0]);
+    console.log(input[1]);
+    console.log(input[2]);
+  }
+  solve([ "a", "b", "c" ])
+  
+  
+function readName(input) {
+    let name = input[0];
+    console.log(name);
+}
+  
+readName(['Apple']);
+ 
+  
