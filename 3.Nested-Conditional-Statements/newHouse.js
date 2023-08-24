d2abad432a01ade0df09fe3a78a59d2023820f5a
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-08-24 17:13:51 +0300

    Update Nested Conditional Statements

--- 3.Nested-Conditional-Statements/newHouse.js
+++ 3.Nested-Conditional-Statements/newHouse.js
@@ -7,33 +7,35 @@ function newHome(input){
   
     if (flower == "Roses"){
         totalPrice = numberFlowers * 5;
+        if (numberFlowers > 80 && flower == "Roses"){
+            totalPrice -= totalPrice * 0.1;
+        }
     } else if (flower == "Dahlias"){
         totalPrice = numberFlowers * 3.8;
+        if (numberFlowers > 90 && flower == "Dahlias"){
+            totalPrice -= totalPrice * 0.15;
+        }
     } else if (flower == "Tulips"){
         totalPrice = numberFlowers * 2.8;
+        if (numberFlowers > 80 && flower == "Tulips"){
+            totalPrice -= totalPrice * 0.15;
+        }
     } else if (flower == "Narcissus"){
         totalPrice = numberFlowers * 3;
+        if (numberFlowers < 120 && flower == "Narcissus"){
+            totalPrice += totalPrice * 0.15;
+        }
     } else if (flower == "Gladiolus"){
         totalPrice = numberFlowers * 2.5;
+        if (numberFlowers < 80 && flower == "Gladiolus"){
+            totalPrice += totalPrice * 0.2;
+        } 
     }
     
-    // discount or increase 
-    if (numberFlowers > 80 && flower == "Roses"){
-        totalPrice -= totalPrice * 0.1;
-    } else if (numberFlowers > 90 && flower == "Dahlias"){
-        totalPrice -= totalPrice * 0.15;
-    } else if (numberFlowers > 80 && flower == "Tulips"){
-        totalPrice -= totalPrice * 0.15;
-    } else if (numberFlowers < 120 && flower == "Narcissus"){
-        totalPrice += totalPrice * 0.15;
-    } else if (numberFlowers < 80 && flower == "Gladiolus"){
-        totalPrice += totalPrice * 0.2;
-    } 
-
     if (budget >= totalPrice){
-        console.log(`Hey, you have a great garden with ${numberFlowers} ${flower} and ${(budget-totalPrice).toFixed(2)} leva left.`)
+        console.log(`Hey, you have a great garden with ${numberFlowers} ${flower} and ${(budget-totalPrice).toFixed(2)} leva left.`);
     } else{
-        console.log(`Not enough money, you need ${(totalPrice - budget).toFixed(2)} leva more.`)
+        console.log(`Not enough money, you need ${(totalPrice - budget).toFixed(2)} leva more.`);
     }
 }
 
