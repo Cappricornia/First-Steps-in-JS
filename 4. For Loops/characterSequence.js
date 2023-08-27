3fb1d18f8b5081041e18ac1c59529618b107cf58
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-08-27 15:45:29 +0300

    Update For Loops

---
+++ 4. For Loops/characterSequence.js
@@ -0,0 +1,11 @@
+function characterSequence(input) {
+
+    let sequence = input[0];
+    
+    for (let i = 0; i < sequence.length; i++) {
+        let letter = sequence[i];
+        console.log(letter);
+      }
+}
+
+characterSequence(["revolution"])
