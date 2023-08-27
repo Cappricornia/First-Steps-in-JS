6fc87e24124b613961ce0a6061dde76c11be80ee
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-08-27 15:07:13 +0300

    Update For Loops

---
+++ 4. For Loops/numbersFrom1ToNWIthStep3.js
@@ -0,0 +1,9 @@
+function numbersFrom1ToNWithStep3(input){
+    let number = Number(input[0]);
+
+    for(let i=1; i<=number; i+=3){
+        console.log(i);
+    }
+}
+numbersFrom1ToNWithStep3(["10"]);
+numbersFrom1ToNWithStep3(["15"]);
