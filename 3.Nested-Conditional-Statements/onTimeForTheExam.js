67d4e8a732ad2627ca7a369b0f642c5fee2c3720
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-08-27 13:43:25 +0300

    Update  Nested Conditional Statements

--- 3.Nested-Conditional-Statements/onTimeForTheExam.js
+++ 3.Nested-Conditional-Statements/onTimeForTheExam.js
@@ -38,6 +38,8 @@ function onTimeForTheExam(input){
  onTimeForTheExam(["9","00","10","30"]);
  onTimeForTheExam(["14","00","13","55"]);
  onTimeForTheExam(["11","30","10","55"]);
+ onTimeForTheExam(["10","00","10","00"]);
+ 
  
 
  
