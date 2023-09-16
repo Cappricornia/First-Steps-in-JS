ae17b1e980d9414fb6187a376b27d0b879d9577b
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-09-16 17:24:22 +0300

    Update Nested Loops

--- 6. Nested Loops/trainTheTrainers.js
+++ 6. Nested Loops/trainTheTrainers.js
@@ -34,16 +34,8 @@ function trainTheTrainers(input){
 }
 
 
-trainTheTrainers(["2",
-"Objects and Classes",
-"5.77",
-"4.23",
-"Dictionaries",
-"4.62",
-"5.02",
-"RegEx",
-"2.88",
-"3.42",
-"Finish"])
+trainTheTrainers(["2","Objects and Classes","5.77","4.23","Dictionaries","4.62","5.02","RegEx","2.88","3.42","Finish"]);
+
+trainTheTrainers(["2","While-Loop","6.00","5.50","For-Loop","5.84","5.66","Finish"]);
 
 
