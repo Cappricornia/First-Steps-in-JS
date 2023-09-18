4b81af2d4e8f9d412b79541668c33eef236a55b6
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-09-18 15:56:04 +0300

    Update Data Types and Variables

--- 7. Data Types and Variables/echoType.js
+++ 7. Data Types and Variables/echoType.js
@@ -1,1 +1,17 @@
+function echoType(input){
+    let data = input;
 
+    if (typeof input === 'number'){
+        console.log(`number\n${data}`);
+    } else if (typeof input === 'string'){
+        console.log(`string\n${data}`);
+    } else{
+        console.log('object\nParameter is not suitable for printing');
+    }
+}
+
+
+
+echoType('Hello, JavaScript!');
+echoType(18);
+echoType(null);
