1d051b4c882479e8963b3d7985b112cbbb5982fd
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-09-20 15:34:17 +0300

    Update Types and Variables

---
+++ 7. Data Types and Variables/specialNumber.js
@@ -0,0 +1,37 @@
+function specialNumber(num){
+    let number = num;
+    let trueOrFalse = "";
+
+    for (let i = 1; i <= number; i++){
+        let currDigit = i;
+        let sum = 0;
+
+        if (currDigit < 10){
+            sum += currDigit;
+            if (sum === 5 || sum === 7 || sum === 11){
+                trueOrFalse = "True";
+            } else{
+                trueOrFalse = "False";
+            }
+
+        } else{
+            currNumTostr = currDigit.toString()
+            for (let j = 0; j<currNumTostr.length; j++){
+                sum += parseInt(currNumTostr[j])
+               
+            }
+
+            if (sum === 5 || sum === 7 || sum === 11){
+                trueOrFalse = "True";
+            } else{
+                trueOrFalse = "False";
+            }
+        }
+        console.log(`${currDigit} -> ${trueOrFalse}`);
+
+    }
+
+}
+
+specialNumber(20)
+
