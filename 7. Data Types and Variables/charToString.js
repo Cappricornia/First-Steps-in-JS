791aeb83bd6a0ba36eba42e8e47034f0ecc4f988
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-09-22 21:56:46 +0300

    Update Data Types and Variables

---
+++ 7. Data Types and Variables/charToString.js
@@ -0,0 +1,12 @@
+function charToString(char1, char2, char3){
+    let firstChar = char1;
+    let secondChar = char2;
+    let thirdChar = char3;
+
+    let generatedString = firstChar + secondChar + thirdChar;
+    console.log(generatedString);
+}
+
+charToString('a','b','c');
+
+charToString('%','2','o');
