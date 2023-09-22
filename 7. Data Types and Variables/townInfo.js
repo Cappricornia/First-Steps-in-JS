41104edbaf108190c08531975375dbb4b78798e0
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-09-22 22:02:36 +0300

    Update Data Types and Variables

---
+++ 7. Data Types and Variables/townInfo.js
@@ -0,0 +1,10 @@
+function townInfo(par1, par2, par3){
+    let town = par1;
+    let population = par2;
+    let area = par3;
+
+    console.log(`Town ${town} has population of ${population} and area ${area} square km.`);
+}
+
+townInfo('Sofia', 1286383, 492);
+townInfo('Plovdiv', 1481353, 512);
