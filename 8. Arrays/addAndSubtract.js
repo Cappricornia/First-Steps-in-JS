79a7131158629eede61b3c84ca041586a5287d19
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-09-24 10:13:29 +0300

    Update Arrays

--- 8. Arrays/addAndSubtract.js
+++ 8. Arrays/addAndSubtract.js
@@ -1,1 +1,27 @@
+function addAndSubtract(arrayInput){
+    let newArray = [];
+    let sumOldArray = 0;
+    let sumNewArray = 0;
 
+    for (let i=0; i < arrayInput.length; i++){
+        let currNum = arrayInput[i];
+        sumOldArray += currNum;
+
+        if (currNum % 2 === 0){
+            currNum += i;
+            sumNewArray += currNum;
+            newArray.push(currNum);
+        } else{
+            currNum -= i;
+            sumNewArray += currNum;
+            newArray.push(currNum);
+        }
+    }
+
+    console.log(newArray);
+    console.log(sumOldArray);
+    console.log(sumNewArray);
+}
+
+
+addAndSubtract([5, 15, 23, 56, 35]);
