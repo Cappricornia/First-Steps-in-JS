67677e8312b2b43f03203ee8aeb4462d6738ad72
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-09-26 14:08:51 +0300

    Update Arrays

---
+++ 8. Arrays/sumEvenNumbers.js
@@ -0,0 +1,17 @@
+function sumEvenNumbers(arr){
+    let sumEven = 0;
+
+    for (let i=0; i< arr.length; i++){
+        let number = Number(arr[i]);
+
+        if (number % 2 === 0){
+            sumEven += number;
+        }
+    }
+
+    console.log(`The sum of the even numbers is: ${sumEven}.`);
+}
+
+
+arr = ['1','2','3','4','5','6'];
+sumEvenNumbers(arr);
