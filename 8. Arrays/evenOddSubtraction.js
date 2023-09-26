3c4936c9e6764ccfbb6438dfd1688806a3bdf7f0
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-09-26 14:32:05 +0300

    Update Arrays

---
+++ 8. Arrays/evenOddSubtraction.js
@@ -0,0 +1,24 @@
+// Write a program that calculates the difference between the sum of the even and the sum of the odd numbers in an array.
+function evenAndOddSubtraction(arr){
+    let sumEven = 0;
+    let sumOdd = 0;
+
+    for (let i=0; i< arr.length; i++){
+        let number = arr[i];
+       
+        if (number % 2 === 0){
+            sumEven += number;
+        } else{
+            sumOdd += number
+        }
+    }
+
+    //console.log(`The sum of the even numbers is: ${sumEven}.`);
+    let diff = sumEven - sumOdd;
+    console.log(`The difference between the sum of the even and odd numbers in the array is ===> ${diff}`);
+}
+
+
+arr = [1,2,3,4,5,6];
+evenAndOddSubtraction(arr);
+
