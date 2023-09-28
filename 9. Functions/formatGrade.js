90d0c8f60d4706d8eb5e75f018a67c1864725f5c
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-09-28 22:02:09 +0300

    Update Functions

--- 9. Functions/formatGrade.js
+++ 9. Functions/formatGrade.js
@@ -1,1 +1,31 @@
+function formatGrade(grade) {
+    let description;
+    let isFailed = false;
+
+    if (grade < 3.00) {
+        description = "Fail";
+        isFailed = true;
+    } else if (grade < 3.50) {
+        description = "Poor";
+    } else if (grade < 4.50) {
+        description = "Good";
+    } else if (grade < 5.50) {
+        description = "Very good";
+    } else {
+        description = "Excellent";
+    }
+
+    if (isFailed){
+        grade = 2;
+        console.log(`${description} (${grade})`);
+    } else{
+        console.log(`${description} (${grade.toFixed(2)})`);
+    }
+}
+
+
+formatGrade(2.80);   
+formatGrade(3.20);   
+formatGrade(4.00);   
+formatGrade(5.70); 
 
