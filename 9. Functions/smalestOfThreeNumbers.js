f5d4ce6b07fa8bf1b6c312d342c2e2215ea875ce
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-09-30 18:07:10 +0300

    Update Functions

---
+++ 9. Functions/smalestOfThreeNumbers.js
@@ -0,0 +1,6 @@
+function smallestOfThreeNumbers(num1, num2, num3) {
+    const smallest = Math.min(num1, num2, num3);
+    console.log(`The smallest number is: ${smallest}`);
+}
+
+smallestOfThreeNumbers(5, 3, 9);  
