6126907f5607992743e3153adb0151d7e9e4bef8
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-10-01 14:12:03 +0300

    Update Functions

---
+++ 9. Functions/oddEvenSums.js
@@ -0,0 +1,18 @@
+function calculateOddEvenSum(n){
+    let evenSum = 0;
+    let oddSum = 0;
+    let stringNumber = String(n);
+
+    for(let i=0; i< stringNumber.length; i++ ){
+        let digit = parseInt(stringNumber[i]);
+        if (digit % 2 === 0){
+            evenSum += digit;
+        } else{
+            oddSum += digit
+        }
+    }
+
+    console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
+}
+
+calculateOddEvenSum( 3495892137259234);
