ade622b6f64ac6a1d5606646cab896804fcf4173
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-10-07 16:22:03 +0300

    Update Object

---
+++ 10. Objects/convertToObject.js
@@ -0,0 +1,23 @@
+function convertToObject(jsonString) {
+    try {
+      const parsedObject = JSON.parse(jsonString);
+      let result = '';
+  
+      for (const key in parsedObject) {
+        result += `${key}: ${parsedObject[key]}\n`;
+      }
+  
+      return result;
+    } catch (error) {
+      console.error("Invalid JSON string:", error.message);
+      return null;
+    }
+  }
+  
+  const jsonString = '{"name": "George", "age": 40, "town": "Sofia"}';
+  //const jsonString = "Simple";  // wrong format
+ // const jsonString = [23, "ABC", false]; // wrong format
+  
+  const result = convertToObject(jsonString);
+  console.log(result);
+  
