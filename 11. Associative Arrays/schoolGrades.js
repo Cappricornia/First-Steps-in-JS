1c62bc43b2b203b33622cc3692b9d8d18065905d
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-10-16 09:03:04 +0300

    Update Associative Arrays

--- 11. Associative Arrays/schoolGrades.js
+++ 11. Associative Arrays/schoolGrades.js
@@ -9,27 +9,15 @@ function schoolGrades(input){
             gradesMap.set(student, grades.map(Number));
         }
     }
-     
-    /*Array.from() to convert the iterator of keys into an array. This is because the sort() method works on arrays, so we need to convert the iterator into an array to use sort().
-    gradesMap.keys(): This uses the keys() method of the Map object. The keys() method returns an iterator 
-    object that contains the keys (in this case, the student names) of the Map. 
-     By calling sort() on the array of student names, we sort the names in lexicographical order (alphabetical order). */
+  
     let sortedStudents = Array.from(gradesMap.keys()).sort();
 
-
-    // It iterates over the elements (in this case, student names) of the sortedStudents array one by one.
     for (const student of sortedStudents) {
-        //  to retrieve the grades associated with that student from the gradesMap
         const grades = gradesMap.get(student);
-        //grades.reduce((sum, grade) => sum + grade, 0) uses the reduce() method to sum up all the grades for the current student. 
-        //The reduce() method accumulates a single result by iterating through an array.
-        //The arrow function (sum, grade) => sum + grade is the function applied to each element during the iteration. sum is the
-        // accumulated sum (initially set to 0 - Providing an initial value of 0), and grade is each grade in the array.
         const averageGrade = (grades.reduce((sum, grade) => sum + grade, 0) / grades.length).toFixed(2);
         console.log(`${student}: ${averageGrade}`);
     }
 }
-
 
 
 const input = [ 'Lilly 4 6 6 5','Tim 5 6','Tammy 2 4 3', 'Tim 6 6'];
