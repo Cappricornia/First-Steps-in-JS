f7328bcc08586dcb4eb0418b8af33d27b70db1db
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-10-22 23:02:32 +0300

    Update Regular Expressions

--- 12. Regular Expressions/matchFullName.js
+++ 12. Regular Expressions/matchFullName.js
@@ -1,1 +1,14 @@
-
+function matchFullNames(names) {
+    const regex = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g;
+    const matchedNames = names.match(regex);
+    
+    if (matchedNames) {
+      console.log(matchedNames.join(', '));
+    } else {
+      console.log('No valid full names found.');
+    }
+  }
+  
+  const names = 'Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, IVan IVanov';
+  matchFullNames(names);
+  
